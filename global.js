c6caf759f81f3a2680ae16cc49c7af342e4d3af4
Angel Thanur <angelthanur@Angels-MBP.modem>
2021-04-07 21:06:29 +0800

    [applicaiton-forward & global] added 'toggleable' quiz and results card, reworked global css styles and js functions

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
+//constants
 const NAVIGATION_ITEMS = [
     {
         label: 'home',
@@ -62,6 +63,7 @@ window.onload = onInit;
  */
 function onInit() {
     constructNavigation();
+    explanationMode();
 }
 
 /**
@@ -74,21 +76,71 @@ function constructNavigation() {
 }
 
 /**
+ * Turns page into 'explanation mode' by showing only the explanation card and hiding the quiz and results card
+ */
+function explanationMode() {
+    hideElementById('quiz-card');
+    hideElementById('results-card');
+    showElementById('explanation-card');
+}
+
+/**
+ * Turns page into 'quiz mode' by showing only the quiz card and hiding the explanation and results card
+ */
+function quizMode() {
+    hideElementById('explanation-card');
+    hideElementById('results-card');
+    showElementById('quiz-card');
+}
+
+/**
+ * Turns page into 'quiz mode' by showing only the results card and hiding the explanation and quiz card
+ */
+function resultsMode() {
+    hideElementById('quiz-card');
+    hideElementById('explanation-card');
+    showElementById('results-card');
+}
+
+
+/**
+ * Evaluates the quiz submission
+ */
+function evaluateSubmission() {
+    //something to send data to server and retrieve results
+    resultsMode();
+}
+
+
+//'Utility' functions
+
+/**
  * Shows elements by Id
- * Retrieves element by Id and adds bootstrap's d-block class to make it visible
+ * Retrieves element by Id, removes bootstrap's d-none class if present and adds bootstrap's d-block class to make it visible
  * @ param - string of elementId
  */
 function showElementById(id) {
     let element = document.getElementById(id);
+
+    element.classList.forEach( className => {
+        if (className === 'd-none') {
+            element.classList.remove("d-none");
+        }
+    });
     element.classList.add("d-block");
 }
 
 /**
  * Hide elements by Id
- * Retrieve elements by Id and add bootstraps' d-none class to hide it
+ * Retrieve elements by Id, resmoves bootstrap's d-block class if present and add bootstraps' d-none class to hide it
  * @ param - string of elementId
  */
 function hideElementById(id) {
     let element = document.getElementById(id);
+    element.classList.forEach( className => {
+        if (className === 'd-block') {
+            element.classList.remove("d-block");
+        }
+    });
     element.classList.add("d-none");
 }
