d2a980d06b0b9f17f98c56a9dc4ef888dea2de5a
22734554 <22734554@student.uwa.edu.au>
2021-08-12 18:46:22 +0800

    prepared files for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: flask db migrate; flask db upgrade; gunicorn networking:app 
--- config.py
+++ config.py
@@ -5,7 +5,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # environment variables setup for SQLAlchemy databases
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestingConfig(Config):
